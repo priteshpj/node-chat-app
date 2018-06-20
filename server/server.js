66d1af1bf7ae822f2ba865d0ef5adeae83d5634a
Pritesh Jain <pritesh.jain@freshworks.com>
2018-06-20 18:53:52 +0530

    port bug fix

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app)
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(port, () => {
   console.log('Server running on port 3000');
 });
