ff0a1403d61228083f5afdd06bac42a2d7128d70
Pritesh Jain <pritesh.jain@freshworks.com>
2018-06-21 14:10:01 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,20 +14,42 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newMessage', {
-  //   from: 'pj',
-  //   text: "Hey, what's up",
-  //   createAt: 124556
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: 'Admin',
+    //   text: 'New user joined',
+    //   createdAt: new Date().getTime()
+    // });
+
   });
+
+
+
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
